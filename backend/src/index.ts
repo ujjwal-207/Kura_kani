3353107602d1ca8ee9b2676c64b91d9775ed10fe
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2025-01-25 21:51:02 +0545

    testing4

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import loginRoutes from "./routes/loginRoutes";
 import { clerkMiddleware } from "@clerk/express";
+import loginRoutes from "./routes/loginRoutes";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
 const corsOptions = {
   origin: ["https://kura-kani-main.vercel.app", "http://localhost:5173"],
-
   credentials: true,
 };
 
@@ -19,6 +19,7 @@ app.use(cors(corsOptions));
 
 app.use(clerkMiddleware());
 app.use("/api", loginRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
