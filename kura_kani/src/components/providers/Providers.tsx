a300eb133b60929b20593aa24ca73d4765a15951
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2025-01-26 15:04:24 +0545

    getorcreate instance in providers

--- kura_kani/src/components/providers/Providers.tsx
+++ kura_kani/src/components/providers/Providers.tsx
@@ -25,7 +25,7 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
         console.log("token", token);
         if (!token) throw new Error("Failed to fetch token");
 
-        const client = new StreamVideoClient({
+        const client = StreamVideoClient.getOrCreateInstance({
           apiKey: apiKey,
           user: {
             id: user?.id,
